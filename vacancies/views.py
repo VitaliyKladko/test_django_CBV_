3fcc7530db4080f82a5cbc38f846d7ae474dd968
VitaliyKladko <vitaliykladko13@gmail.com>
2023-02-26 18:18:38 +0300

    add vacancies route

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
+from vacancies.models import Vacancy
+
+
 # from django.shortcuts import render
 
 
@@ -8,3 +12,24 @@ def index(request):
 
 def hello(request):
     return HttpResponse('Hello, Dima')
+
+
+def index_vacancies(request):
+    """
+    В request (класс) есть практически вся инфа о поступившем запросе
+    objects - это формально ORM и в дальнейшем позволит нам обращаться к данным из БД
+    """
+    if request.method == 'GET':
+        vacancies = Vacancy.objects.all()
+
+        response = []
+        for vacancy in vacancies:
+            response.append(
+                {
+                    'id': vacancy.id,
+                    'text': vacancy.text,
+                }
+            )
+        # safe=False позволяет "скушать" JsonResponse словарь, говоря, что ничего не сломается при серриализации и
+        # отключи все проверки при переводе в json
+        return JsonResponse(response, safe=False)
